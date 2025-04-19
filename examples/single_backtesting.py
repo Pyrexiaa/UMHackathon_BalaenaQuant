b3780861ad1bde5ddec709c1de4aa685876a66bc
AsynchronousNotAvailable <whleexd@gmail.com>
2025-04-20 00:22:01 +0800

    tcn scaler files

--- examples/single_backtesting.py
+++ examples/single_backtesting.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 if __name__ == "__main__":
     
-    bt = Backtester(data=BTC_DATA, strategy=MLStrategy(get_model("tcn")), strategy_name="tcn",
+    bt = Backtester(data=BTC_DATA, strategy=MLStrategy(get_model("tcn")), strategy_name="TCN",
                     mode="geometric", entry_exit_logic="mean_reversion")
     # bt = Backtester(data=BTC_DATA, strategy=MLStrategy(get_model("TCN")),
     #             mode="geometric", entry_exit_logic="mean_reversion")
