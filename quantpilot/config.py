bd0532344221edffa92579d34df988c156752c59
huiern214 <huiern214@gmail.com>
2025-04-20 01:44:45 +0800

    fixed xgboost model path

--- quantpilot/config.py
+++ quantpilot/config.py
@@ -21,7 +21,6 @@ class XGBConfig:
     XGB_SCALER_PATH = "quantpilot/models_weights/xgboost2/scaler.pkl"
     
     # assumption 10 model
-<<<<<<< HEAD
     # XGB_MODEL_PATH = "quantpilot/models_weights/xgboost_final/xgboost_model.pkl"
     # XGB_SCALER_PATH = "quantpilot/models_weights/xgboost_final/scaler.pkl"
 
@@ -30,10 +29,9 @@ class XGBConfig:
 
     # XGB_MODEL_PATH = "quantpilot/models_weights/xgboost_final_testing/xgboost_model.pkl"
     # XGB_SCALER_PATH = "quantpilot/models_weights/xgboost_final_testing/scaler.pkl"
-=======
-    XGB_MODEL_PATH = "quantpilot/models_weights/xgboost_final/xgboost_model.pkl"
-    XGB_SCALER_PATH = "quantpilot/models_weights/xgboost_final/scaler.pkl"
->>>>>>> c015e6e0b4a3506fdb9444f8d3c9112975c509f8
+
+    # XGB_MODEL_PATH = "quantpilot/models_weights/xgboost_final/xgboost_model.pkl"
+    # XGB_SCALER_PATH = "quantpilot/models_weights/xgboost_final/scaler.pkl"
 
     XGB_FEATURE_PATH = [
         "price_change_1",
